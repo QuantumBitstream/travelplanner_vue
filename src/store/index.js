8aade7234a80643b12132ea75d4d235580904fed
testingforforwhat <2273664Z@student.gla.ac.uk>
2024-11-07 20:33:51 +0800

    create baseStore

---
+++ src/store/index.js
@@ -0,0 +1,60 @@
+// 从 vuex 模块 导入 创建仓库的方法
+import { createStore } from 'vuex'
+
+// import Vue from 'vue'
+// import Vuex from 'vuex'
+import getters from './getters'
+
+
+
+/**
+ *
+ * Vue.use(Vuex) 是在 Vue 2 中使用 Vuex 这个状态管理库的一个关键步骤。
+ * 它的作用是将 Vuex 插件安装到 Vue 框架中，使得 Vue 组件能够使用 Vuex 提供的状态管理功能。
+ *
+ * 在组件中，你可以通过 this.$store 来访问 Vuex Store，并通过相应的方法来读取状态、提交突变或者发起动作。
+ *
+ *
+ * Vue.use(Vuex) 是为在 Vue 2 中使用 Vuex 插件而必要的。
+ * 它的作用是将 Vuex 安装为 Vue 的插件，使全局的状态管理在组件中可用。
+ * 而在 Vue 3 中，这个步骤已经被内部简化和优化。
+ *
+ * 在 Vue 3 中，Vue.use(Vuex) 不再是必需的操作，因为 Vuex 被直接导入和使用，不需要显式安装插件
+ *
+ *
+ */
+// Vue.use(Vuex)
+
+
+/**
+ *
+ * 创建 Vuex Store，用于定义你的应用状态、突变、动作和 getters。
+ *
+ * @type {Store<{}>}
+ */
+// 创建 总仓库 并且导出
+// export default createStore({
+export default createStore({
+// // const store = new Vuex.Store({
+//     // 各组件实例的data 迁移到这儿
+//     state: {
+//
+//     },
+//     // 各组件实例的(同步)方法 迁移到这儿
+//     mutations: {
+//     },
+//     // 各组件实例的(异步)方法 迁移到这儿
+//     actions: {
+//         /**
+//          * Error: [vuex] actions should be function or object with "handler" function but
+//          */
+//
+//     },
+    // 总仓库 不实现任何状态管理 (没有 state mutations actions)         只合并子仓库
+    modules: {
+
+    },
+    getters
+})
+
+// export default store
---
+++ src/utils/request.js
@@ -0,0 +1,56 @@
+
+
+/*在这个文件中我们对请求和响应进行封装*/
+
+
+import axios from "axios";
+import router from "@/router";
+
+
+const Config = require('../settings');
+
+/**
+ * qs 模块是一个用于序列化和反序列化 URL 查询字符串的库。
+ * 通常与 Axios 等 HTTP 请求库一起使用，用于处理 URL 查询参数的编码和解码
+ */
+// import qs from 'qs';
+
+// import router from "@/router/index.js";
+
+
+// 创建axios异步请求对象 进行初始化 配置
+// 创建axios实例
+const service = axios.create(
+    {
+        baseURL: "/api/",
+        // 发送 post/put/patch/delete 请求之前 对 data 参数进行统一处理
+        // transformRequest(data){
+        //     return qs.stringify(data)
+        // }
+        timeout: Config.timeout // 请求超时时间
+
+    }
+)
+
+// 配置 axios 请求 拦截器
+service.interceptors.request.use( config => {
+
+    return config
+})
+
+// 配置 axios 返回 拦截器
+service.interceptors.response.use( response => {
+
+    if (response.status === 401) {
+        router.push( { name: 'Error', params : { message: '请先登录' } } );
+    } else if (response.status === 402) {
+        router.push( { name: 'Error', params : { message: '登录过期，请重新登陆' } } );
+    } else if (response.status === 403) {
+        router.push( { name: 'Error', params : { message: '没有操作权限,请联系管理员' } } );
+    }
+    return response
+})
+
+
+// 导出
+export default service
