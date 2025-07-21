e499f9d9b69cb9d391023eda1a5d05fb1331f7cc
testingforforwhat <2273664Z@student.gla.ac.uk>
2025-07-21 19:29:09 +0800

    refactor(Login.vue + utils.js): 登录页 vue3 element-plus vuex 页面美化 代码优化 添加注释
    Closes #2

--- src/api/index.js
+++ src/api/index.js
@@ -5,6 +5,13 @@ import * as loginApi from './modules/login';
 // 如果有别的模块，继续在这里引入
 // import * as productApi from './modules/product';
 
+
+/**
+ *
+ * 为了方便调用，我们将所有模块的 API 在这里整合并导出。
+ *
+ * @type {{user: {}, login: {}}}
+ */
 const api = {
     user: userApi,
     login: loginApi
